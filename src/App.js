2be04d3993c760fc8c792e2efa961abd0a4fe59c
Aman Singh Bhogal <45331015+AmanSinghBhogal@users.noreply.github.com>
2022-08-10 15:51:53 +0530

    Added Home, NavBar and Footer Commponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './Components/Footer';
 import NavBar from './Components/NavBar';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <Footer />
     </div>
   );
 }
